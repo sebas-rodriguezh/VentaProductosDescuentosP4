8e5276e0da612784485b921755afba3e609f8fa5
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2026-03-27 16:13:59 -0600

    first commit

---
+++ src/main/java/com/example/ideaproductoscomprasdescuentos/IdeaProductosComprasDescuentosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideaproductoscomprasdescuentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaProductosComprasDescuentosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaProductosComprasDescuentosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ideaproductoscomprasdescuentos/IdeaProductosComprasDescuentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideaproductoscomprasdescuentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaProductosComprasDescuentosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
